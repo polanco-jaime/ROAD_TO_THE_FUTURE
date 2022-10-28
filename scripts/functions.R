fc3736b0ac2e38aa39a95d5c1da3e1ecf6529882
JAIME ANDRES POLANCO JIMENEZ <jaime.polanco@javeriana.edu.co>
2022-10-27 20:52:34 -0500

    ok 1

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,3 +1,15 @@
+#load("C:/Users/USER/Desktop/DID roads/did_roads/did_roads.RData")
+lista = c('readr','readxl','sqldf','plyr', 
+          'did' , 'arrow',  'plyr', 'ggplot2',
+          'dplyr','fixest' , 'gargle' , 'stringr'
+          #, 'bigrquery' 
+)
+for (i in 1:length(lista) ) {
+  if(lista[i] %in% rownames(installed.packages()) == FALSE) {
+    install.packages(lista[i])
+  }
+  lapply(lista[i], library, character.only = TRUE)
+}
 
 ###########basura#############
 #picture buffers
